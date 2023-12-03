7f6a13e19178662fc6cb198a67612ad36f484bce
adad22ro <133160053+adad22ro@users.noreply.github.com>
2023-12-03 18:52:33 +0000

    Deploying to gh-pages from @ adad22ro/goit-js-hw-10@8ac984f025a05f56714c075f8094274ebcb4ca97 🚀

--- src/loader.js
+++
@@ -1,6 +0,0 @@
-export function toggleLoader(visible) {
-  const loaderContainer = document.querySelector('.loader-container');
-  if (loaderContainer) {
-    loaderContainer.style.display = visible ? 'flex' : 'none';
-  }
-}
